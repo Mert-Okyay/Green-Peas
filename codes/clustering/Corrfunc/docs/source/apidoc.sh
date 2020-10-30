5ceb268a5149522b44a5180e04b59b83282105d4
Mert Okyay <mokyay@alfaromeo.physics.miami.edu>
2020-10-30 03:17:50 -0400

    Revert "Added research files"
    This reverts commit 72a6e5ea530ffdb78f9e8ba3733eea0e52539c06.

--- codes/clustering/Corrfunc/docs/source/apidoc.sh
+++
@@ -1,6 +0,0 @@
-# bash
-
-if ! python -c 'import numpydoc'; then easy_install --user numpydoc; fi
-if ! python -c 'import sphinx'; then easy_install --user sphinx; fi
-
-sphinx-apidoc -H "Comprehensive API reference" -M -f -o source/api/ ../Corrfunc/
